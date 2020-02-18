8f9dc33c699f668a18c961666e68ba9a7ba7fdc0
Iago Fernandes <iagofernandesbc@gmail.com>
2020-02-17 23:40:27 -0300

    4.30. Modelando e implementando a inclusão de recursos de restaurantes

--- src/main/java/br/com/clickfood/api/controller/CozinhaController.java
+++ src/main/java/br/com/clickfood/api/controller/CozinhaController.java
@@ -3,7 +3,6 @@ package br.com.clickfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.clickfood.domain.exception.EntidadeEmUsoException;
+import br.com.clickfood.domain.exception.EntidadeNaoEncontradaException;
 import br.com.clickfood.domain.model.Cozinha;
 import br.com.clickfood.domain.repository.CozinhaRepository;
+import br.com.clickfood.domain.service.CozinhaService;
 
 @RestController
 @RequestMapping("/cozinhas")
@@ -24,6 +26,9 @@ public class CozinhaController {
 
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
+
+	@Autowired
+	private CozinhaService cozinhaService;
 
 	@GetMapping
 	public List<Cozinha> listar() {
@@ -34,35 +39,35 @@ public class CozinhaController {
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
 		Cozinha cozinha = cozinhaRepository.buscar(id);
 		if (cozinha != null) {
-			return ResponseEntity.ok(cozinha);
+			return ResponseEntity.status(HttpStatus.OK).body(cozinha);
 		}
-		return ResponseEntity.notFound().build();
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 
 	@PostMapping
 	public Cozinha salvar(@RequestBody Cozinha cozinha) {
-		return cozinhaRepository.salvar(cozinha);
+		return cozinhaService.salvar(cozinha);
 	}
 
 	@PutMapping
 	public ResponseEntity<Cozinha> atualizar(@RequestBody Cozinha cozinha) {
 		if (cozinha != null && cozinha.getId() != null) {
-			cozinha = cozinhaRepository.salvar(cozinha);
-			return ResponseEntity.ok(cozinha);
+			cozinha = cozinhaService.salvar(cozinha);
+			return ResponseEntity.status(HttpStatus.OK).body(cozinha);
 		}
-		return ResponseEntity.notFound().build();
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Cozinha> remover(@PathVariable Long id) {
 		try {
-			Cozinha cozinha = cozinhaRepository.buscar(id);
-			if (cozinha != null) {
-				cozinhaRepository.remover(cozinha);
-				return ResponseEntity.noContent().build();
-			}
-			return ResponseEntity.notFound().build();
-		} catch (DataIntegrityViolationException e) {
+			cozinhaRepository.remover(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+		} catch (EntidadeEmUsoException e) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
 	}
---
+++ src/main/java/br/com/clickfood/domain/service/CozinhaService.java
@@ -0,0 +1,37 @@
+package br.com.clickfood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import br.com.clickfood.domain.exception.EntidadeEmUsoException;
+import br.com.clickfood.domain.exception.EntidadeNaoEncontradaException;
+import br.com.clickfood.domain.model.Cozinha;
+import br.com.clickfood.domain.repository.CozinhaRepository;
+
+@Service
+public class CozinhaService {
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	public Cozinha salvar(Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
+	}
+
+	public void remover(Long id) {
+		try {
+			cozinhaRepository.remover(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Cozinha de código %d não existe.", id));
+
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(
+					String.format("Cozinha de código %d não pode ser removida, pois está em uso.", id));
+		}
+	}
+
+}
